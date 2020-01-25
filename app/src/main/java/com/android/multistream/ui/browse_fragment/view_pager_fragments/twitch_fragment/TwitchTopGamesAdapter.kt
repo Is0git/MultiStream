3586ac59789193491151bbe10a08c5e7288ac5da
Danielius <dnlspasin@gmail.com>
2020-01-25 21:01:00 +0200

    item highlight animation in top games recyclerview

--- app/src/main/java/com/android/multistream/ui/browse_fragment/view_pager_fragments/twitch_fragment/TwitchTopGamesAdapter.kt
+++ app/src/main/java/com/android/multistream/ui/browse_fragment/view_pager_fragments/twitch_fragment/TwitchTopGamesAdapter.kt
@@ -1,15 +1,23 @@
 package com.android.multistream.ui.browse_fragment.view_pager_fragments.twitch_fragment
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.TimeInterpolator
+import android.annotation.SuppressLint
+import android.view.*
+import android.view.animation.BounceInterpolator
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.recyclerview.widget.RecyclerView
+import com.android.multistream.R
 import com.android.multistream.databinding.TwitchTopGamesListBinding
 import com.android.multistream.di.MainActivity.browse_fragment.view_pager_fragments.twitch_fragment.TwitchFragmentGamesScope
 import com.android.multistream.network.twitch.models.v5.TopItem
+import kotlinx.android.synthetic.main.twitch_top_games_list.view.*
 import javax.inject.Inject
 
 @TwitchFragmentGamesScope
-class TwitchTopGamesAdapter @Inject constructor() : RecyclerView.Adapter<TwitchTopGamesAdapter.MyViewHolder>() {
+class TwitchTopGamesAdapter @Inject constructor() :
+    RecyclerView.Adapter<TwitchTopGamesAdapter.MyViewHolder>() {
     lateinit var clickListener: OnGameCategoryListener
     var list: List<TopItem>? = null
         set(value) {
@@ -18,12 +26,88 @@ class TwitchTopGamesAdapter @Inject constructor() : RecyclerView.Adapter<TwitchT
             notifyItemRangeChanged(begin, value?.size!! - 1)
         }
 
-    class MyViewHolder(val binding: TwitchTopGamesListBinding) : RecyclerView.ViewHolder(binding.root)
+    class MyViewHolder(val binding: TwitchTopGamesListBinding) :
+        RecyclerView.ViewHolder(binding.root), GestureDetector.OnGestureListener,
+        View.OnTouchListener {
+        lateinit var scaleYAnimation: ObjectAnimator
+        lateinit var scaleXAnimation: ObjectAnimator
+        var gestureListener: GestureDetector
+        lateinit var animatorSet: AnimatorSet
+        lateinit var elevationAnim: ObjectAnimator
+
+        init {
+            setupAnimators()
+            gestureListener = GestureDetector(binding.root.context, this)
+            binding.root.setOnTouchListener(this)
+        }
+
+        private fun setupAnimators() {
+
+            scaleXAnimation = ObjectAnimator.ofFloat(binding.root, "scaleX", 1f, 1.30f)
+            scaleYAnimation = ObjectAnimator.ofFloat(binding.root, "scaleY", 1f, 1.30f)
+            elevationAnim = ObjectAnimator.ofFloat(binding.root, "elevation", 1f, 10f)
+            animatorSet = AnimatorSet().apply {
+                interpolator = FastOutSlowInInterpolator()
+                playTogether(
+                    scaleYAnimation, scaleXAnimation, elevationAnim
+                )
+                duration = 400
+            }
+        }
+
+        override fun onShowPress(e: MotionEvent?) {
+            if (e?.action != MotionEvent.ACTION_SCROLL)     animatorSet.start()
+
+
+        }
+
+        override fun onSingleTapUp(e: MotionEvent?): Boolean {
+            return true
+        }
+
+        override fun onDown(e: MotionEvent?): Boolean {
+            return true
+        }
+
+        override fun onFling(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            return true
+        }
+
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            animatorSet.reverse()
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent?) {
+
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+            gestureListener.onTouchEvent(event)
+            if (event?.action == 1) {
+                animatorSet.reverse()
+            }
+            return true
+        }
+    }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
-        val binding = TwitchTopGamesListBinding.inflate(LayoutInflater.from(parent.context), parent, false).also { it.clickListener = this.clickListener }
+        val binding =
+            TwitchTopGamesListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//                .also { it.clickListener = this.clickListener }
         return MyViewHolder(binding)
     }
 
