cfa52e5dbbb593386c3057116f447bd97e7be685
Danielius <dnlspasin@gmail.com>
2020-01-25 13:40:32 +0200

    smarter load page detect, load pages before 5 items to the end

--- app/src/main/java/com/android/multistream/ui/browse_fragment/view_pager_fragments/twitch_fragment/TwitchFragment.kt
+++ app/src/main/java/com/android/multistream/ui/browse_fragment/view_pager_fragments/twitch_fragment/TwitchFragment.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.multistream.R
 import com.android.multistream.databinding.TwitchGamesFragmentPageBinding
@@ -22,9 +23,11 @@ import javax.inject.Inject
 class TwitchFragment : DaggerFragment(), OnGameCategoryListener {
 
     lateinit var binding: TwitchGamesFragmentPageBinding
-    @Inject lateinit var factory: ViewModelFactory
+    @Inject
+    lateinit var factory: ViewModelFactory
     lateinit var twitchFragmentViewModel: TwitchFragmentViewModel
-    @Inject lateinit var topGamesAdapter: TwitchTopGamesAdapter
+    @Inject
+    lateinit var topGamesAdapter: TwitchTopGamesAdapter
     lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,11 +35,13 @@ class TwitchFragment : DaggerFragment(), OnGameCategoryListener {
         savedInstanceState: Bundle?
     ): View? {
         Log.d("TWITCHFRAGMENT", "onCreate")
-        twitchFragmentViewModel = ViewModelProviders.of(this, factory).get(TwitchFragmentViewModel::class.java)
+        twitchFragmentViewModel =
+            ViewModelProviders.of(this, factory).get(TwitchFragmentViewModel::class.java)
         binding = TwitchGamesFragmentPageBinding.inflate(inflater, container, false)
         setupList()
         setupObservers()
-      return  binding.root
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -52,18 +57,28 @@ class TwitchFragment : DaggerFragment(), OnGameCategoryListener {
 
     private fun setupList() {
         binding.apply {
-            topTwitchGamesList.adapter = topGamesAdapter.also { it.clickListener = this@TwitchFragment }
+            topTwitchGamesList.adapter =
+                topGamesAdapter.also { it.clickListener = this@TwitchFragment }
             topTwitchGamesList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
                 override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                     super.onScrolled(recyclerView, dx, dy)
-                    if (!recyclerView.canScrollVertically(1) && twitchFragmentViewModel.getPaginationState() != PageLoadingStates.LOADING) twitchFragmentViewModel.loadPage()
+
+                    if (dy > 0 && twitchFragmentViewModel.getPaginationState() != PageLoadingStates.LOADING) {
+                        val itemsCount = (binding.topTwitchGamesList.layoutManager as GridLayoutManager).itemCount
+                        val lastVisibleItem = (binding.topTwitchGamesList.layoutManager as GridLayoutManager).findLastVisibleItemPosition()
+
+                        if (itemsCount - lastVisibleItem - 1 < 4 ) twitchFragmentViewModel.loadPage()
+
+                    }
                 }
             })
         }
     }
 
     private fun setupObservers() {
-        twitchFragmentViewModel.paginationLiveData.observe(viewLifecycleOwner, Observer { topGamesAdapter.list = it})
+        twitchFragmentViewModel.paginationLiveData.observe(
+            viewLifecycleOwner,
+            Observer { topGamesAdapter.list = it })
     }
 
     override fun onGameClick(
@@ -74,7 +89,14 @@ class TwitchFragment : DaggerFragment(), OnGameCategoryListener {
         boxImage: String?,
         gameId: String?
     ) {
-       val directions = BrowseFragmentDirections.actionBrowseFragmentToGameChannelsFragment(platformType, viewers, channels, name, boxImage, gameId)
+        val directions = BrowseFragmentDirections.actionBrowseFragmentToGameChannelsFragment(
+            platformType,
+            viewers,
+            channels,
+            name,
+            boxImage,
+            gameId
+        )
         navController.navigate(directions)
     }
 
