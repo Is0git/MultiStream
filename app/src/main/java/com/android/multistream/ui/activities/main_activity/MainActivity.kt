58237f7758ce7ca89856268f2f141fc300cb3736
Danielius <dnlspasin@gmail.com>
2020-02-15 19:05:35 +0200

    material navigation drawer

--- app/src/main/java/com/android/multistream/ui/activities/main_activity/MainActivity.kt
+++ app/src/main/java/com/android/multistream/ui/activities/main_activity/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.android.multistream.ui.activities.main_activity
 
-import android.graphics.PixelFormat
 import android.os.Bundle
+import android.transition.Transition
+import android.transition.TransitionInflater
+import android.transition.TransitionManager
+import android.transition.TransitionSet
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.get
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.android.multistream.R
 import com.android.multistream.databinding.ActivityMainBinding
+import com.android.multistream.utils.ScreenUnit
 import com.android.multistream.utils.TWITCH_TOKEN
 import com.android.multistream.utils.ViewModelFactory
 import com.android.multistream.utils.twitchAPI.uriQuery
@@ -22,14 +33,33 @@ class MainActivity : DaggerAppCompatActivity() {
 
     lateinit var mainActivityViewModel: MainActivityViewModel
 
+    val transition by lazy {
+        TransitionInflater.from(this)
+            .inflateTransition(R.transition.games_list_expand_transition)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         mainActivityViewModel = ViewModelProviders.of(this, viewModelFactory).get(
-            MainActivityViewModel::class.java)
+            MainActivityViewModel::class.java
+        )
+        binding.settingsIcon.setOnClickListener {
+            TransitionManager.beginDelayedTransition(binding.root as ViewGroup, transition)
 
+            if (binding.navigation.isVisible)  {
+                binding.navigation.translationX = -350f
+
+            }
+            else {
+                binding.navigation.layoutParams = ConstraintLayout.LayoutParams(ScreenUnit.convertDpToPixel(200f), MATCH_PARENT)
+                binding.navigation.visibility = View.VISIBLE
+            }
+
+
+        }
 //        binding.button.setOnClickListener {
 //            val intent = Intent(Intent.ACTION_VIEW, twitchAuthPage.toUri())
 //            startActivity(intent)
@@ -45,7 +75,7 @@ class MainActivity : DaggerAppCompatActivity() {
 
     private fun authorizeTwitch() {
         val token: String? = intent.data?.let { uriQuery(it.toString()) }
-        token?.let {  mainActivityViewModel.authorize(TWITCH_TOKEN, token) }
+        token?.let { mainActivityViewModel.authorize(TWITCH_TOKEN, token) }
     }
 
 
